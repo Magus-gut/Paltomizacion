2902006ba4ad69dd64db68022348fe9374fa61f1
Andrés Martínez <amartnez@uc.cl>
2023-06-20 23:43:21 -0400

    sequía e inundaciones

--- datos.py
+++ datos.py
@@ -38,3 +38,14 @@ T = dft.to_dict()
 print(f"max pp: {max(pp.values())}")
 print(f"max evap: {max(evap.values())}")
 print(f"max T: {max(T.values())}")
+
+
+data_inundacion = {'Value': [35] * 364}  # Creates a list of 364 35's
+df_inunda = pd.DataFrame(data_inundacion)
+
+data_sequía = {'Value': [0] * 364}  # Creates a list of 364 0's
+df_sequia = pd.DataFrame(data_sequía)
+
+
+# print(df_inunda)
+# print(df_sequia)
